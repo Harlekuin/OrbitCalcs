9f4ba516e61b495700b6a3a2a3ad518c7f1720be
Harlekuin <tommalkin28@gmail.com>
2016-10-31 20:04:07 +1100

    Add Radius of the Earth and start circular orbit

--- OrbitCalcs.py
+++ OrbitCalcs.py
@@ -7,3 +7,14 @@ MASS_OF_EARTH = M = astropy.constants.M_earth
 
 print(G)
 print(M)
+print(M.to('kg'))
+
+RADIUS_OF_EARTH = astropy.constants.R_earth
+print(RADIUS_OF_EARTH)
+
+#details of a circular orbit
+orbit_km = 200
+total_height = orbit_km + RADIUS_OF_EARTH.to('km')
+
+#V^2 = GM(2/r - 1/a)
+r = total_height
