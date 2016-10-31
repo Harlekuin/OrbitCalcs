ea4d9939f4f32c9ed0ac9f1ec1cd853fc210b1d4
Harlekuin <tommalkin28@gmail.com>
2016-11-01 00:29:03 +1100

    Add working circular orbit equation

--- OrbitCalcs.py
+++ OrbitCalcs.py
@@ -1,20 +1,21 @@
 import numpy
 import scipy.constants
 import astropy.constants
+from astropy import units as u
 
-GRAVITATIONAL_CONSTANT = G = scipy.constants.G
+GRAVITATIONAL_CONSTANT = G = astropy.constants.G
 MASS_OF_EARTH = M = astropy.constants.M_earth
 
-print(G)
-print(M)
-print(M.to('kg'))
-
 RADIUS_OF_EARTH = astropy.constants.R_earth
-print(RADIUS_OF_EARTH)
 
 #details of a circular orbit
-orbit_km = 200
-total_height = orbit_km + RADIUS_OF_EARTH.to('km')
+#AVERAGE_HEIGHT = 1.496e11 * u.meter + RADIUS_OF_EARTH.to('m')
+#CURRENT_HEIGHT = 1 * u.meter + RADIUS_OF_EARTH.to('m')
+CURRENT_HEIGHT = AVERAGE_HEIGHT = 8400 * u.kilometer
+
 
 #V^2 = GM(2/r - 1/a)
-r = total_height
+
+V = ((G * M) * ((2/CURRENT_HEIGHT) - (1/AVERAGE_HEIGHT)))**.5
+
+print(V.to('m/s'))
