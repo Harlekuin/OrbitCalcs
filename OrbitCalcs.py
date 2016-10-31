12bcac0afebe454638ebb470d916d72d6e0b58d7
Harlekuin <tommalkin28@gmail.com>
2016-10-31 17:54:13 +1100

    Add the Mass of earth constant

--- OrbitCalcs.py
+++ OrbitCalcs.py
@@ -1,7 +1,9 @@
 import numpy
 import scipy.constants
-import astropy
+import astropy.constants
 
 GRAVITATIONAL_CONSTANT = G = scipy.constants.G
+MASS_OF_EARTH = M = astropy.constants.M_earth
 
 print(G)
+print(M)
